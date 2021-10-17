4cee9d5c5f81e79f17efbd14df7d8181230626dc
samrand <sammajnoni@gamil.com>
2021-10-18 00:16:36 +0330

    router and create components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { useEffect,useState } from 'react' 
 import './App.css';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Layout from './Layout/Layout';
+import React from 'react';
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <Header/>
+
+      <Layout/>
+      
+     <Footer/>
     </div>
+    
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PopularMoviesSlider from "../component/PopularMoviesSlider";
+
+
+export default function Home() {
+    return (
+        <div>
+         <PopularMoviesSlider/>
+        </div>
+    )
+}
